58485a94e82539c314d7302b09390857e3d999c7
protamlan <alanmendoza6160@gmail.com>
2022-09-18 19:38:09 -0700

    updated

--- app/src/main/java/com/example/panache/MainActivity.kt
+++ app/src/main/java/com/example/panache/MainActivity.kt
@@ -10,15 +10,48 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val answer1 = findViewById<TextView>(R.id.a1)
+        val answer2 = findViewById<TextView>(R.id.a2)
+        val answer3 = findViewById<TextView>(R.id.a3)
         val question = findViewById<TextView>(R.id.flashcard_question)
-        val answer = findViewById<TextView>(R.id.flashcard_answer)
+        val correctanswer = findViewById<TextView>(R.id.flashcard_answer)
+        val mode = findViewById<TextView>(R.id.mode)
+        /*val background = findViewById<TextView>(R.id.background)
+        background.setOnClickListener{
+            answer1.visibility = View.VISIBLE
+            answer2.visibility = View.VISIBLE
+            answer3.visibility = View.VISIBLE
+            question.visibility = View.INVISIBLE
+            correctanswer.visibility = View.VISIBLE
+        }*/
+        //startActivity(MyActivity.class, getIntent())
         question.setOnClickListener {
             question.visibility = View.INVISIBLE
-            answer.visibility = View.VISIBLE
+            correctanswer.visibility = View.VISIBLE
         }
-        answer.setOnClickListener {
+        correctanswer.setOnClickListener {
             question.visibility = View.VISIBLE
-            answer.visibility = View.INVISIBLE
+            correctanswer.visibility = View.INVISIBLE
         }
+
+        answer1.setOnClickListener {
+            findViewById<View>(R.id.a1).setBackgroundColor(getResources().getColor(R.color.red, null))
+            findViewById<View>(R.id.a3).setBackgroundColor(getResources().getColor(R.color.green, null))
+        }
+        answer2.setOnClickListener {
+            answer2.visibility = View.VISIBLE
+            correctanswer.visibility = View.INVISIBLE
+            findViewById<View>(R.id.a2).setBackgroundColor(getResources().getColor(R.color.red, null))
+            findViewById<View>(R.id.a3).setBackgroundColor(getResources().getColor(R.color.green, null))
+        }
+        answer3.setOnClickListener {
+            findViewById<View>(R.id.a3).setBackgroundColor(getResources().getColor(R.color.green, null))
+        }
+        mode.setOnClickListener {
+            answer1.visibility = View.INVISIBLE
+            answer2.visibility = View.INVISIBLE
+            answer3.visibility = View.INVISIBLE
+        }
+
     }
 }
